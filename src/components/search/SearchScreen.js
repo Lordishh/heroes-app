4431e38b56ebf146f0704287fd78166858a49663
Enuel <enuel.monzon@gmail.com>
2022-02-20 02:58:03 -0300

    add router dom

---
+++ src/components/search/SearchScreen.js
@@ -0,0 +1,7 @@
+export const SeachScreen = () => {
+  return (
+    <div>
+      <h1>SeachScreen</h1>
+    </div>
+  );
+};
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,23 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import { DcScreen } from "../components/dc/DcScreen";
+import { LoginScreen } from "../components/login/LoginScreen";
+import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { SeachScreen } from "../components/search/SearchScreen";
+
+import { Navbar } from "../components/ui/Navbar";
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MarvelScreen />} />
+        <Route path="/marvel" element={<MarvelScreen />} />
+        <Route path="/dc" element={<DcScreen />} />
+        <Route path="/search" element={<SeachScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
